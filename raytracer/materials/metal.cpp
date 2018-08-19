edcb4794982f43306a0572a4599cce4965c60c85
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-19 17:15:28 +0100

    metal roughness

--- raytracer/materials/metal.cpp
+++ raytracer/materials/metal.cpp
@@ -1,9 +1,9 @@
 #include "metal.h"
-#include "glm/glm.hpp"
+#include "glm/gtc/random.hpp"
 
 bool metal::scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
     vec3 reflected = reflect(normalize(r_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
+    scattered = ray(rec.p, reflected + ballRand(fuzz));
     attenuation = albedo;
     return (dot(scattered.direction(), rec.normal) > 0);
 }
--- raytracer/materials/metal.h
+++ raytracer/materials/metal.h
@@ -2,9 +2,10 @@
 #include "material.h"
 
 struct metal : public material {
-    metal(const vec3& a) : albedo(a) {}
+    metal(const vec3& a, float fuzz) : albedo(a), fuzz(clamp(fuzz, 0.0f, 1.0f)) {}
     
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const;
     
     vec3 albedo;
+    float fuzz;
 };
