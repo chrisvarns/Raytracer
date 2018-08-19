c8de2267143252c956672863982b86fec12c76c7
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-19 12:12:05 +0100

    Diffuse reflectance

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -2,11 +2,13 @@
 #include "hitablelist.h"
 #include "raytracer.h"
 #include "sphere.h"
+#include "glm/gtc/random.hpp"
 
 vec3 color(const ray& r, hitable* world) {
     hit_record rec;
-    if(world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return vec3(0.5)*(rec.normal+vec3(1));
+    if(world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + ballRand<float>(1);
+        return vec3(0.5)*color(ray(rec.p, target-rec.p), world);
     }
     else {
         vec3 unit_direction = normalize(r.direction());
@@ -18,7 +20,7 @@ vec3 color(const ray& r, hitable* world) {
 void redraw(U8* outPtr, int width, int height) {
     
 #if DEBUG
-    const int ns = 10;
+    const int ns = 4;
 #else
     const int ns = 100;
 #endif
@@ -39,6 +41,7 @@ void redraw(U8* outPtr, int width, int height) {
                 col += color(r, world);
             }
             col /= ns;
+            col = sqrt(col);
             *(outPtr++) = U8(255.99 * col.r);
             *(outPtr++) = U8(255.99 * col.g);
             *(outPtr++) = U8(255.99 * col.b);
