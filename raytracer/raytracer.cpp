c09df2a347a8dcff65375e347f72a6a0857872e6
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-19 11:30:28 +0100

    End of chapter 5, hitable list etc

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -1,27 +1,17 @@
 #include "raytracer.h"
-#include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    if(discriminant < 0)
-        return -1;
-    return (-b - sqrt(discriminant)) / (2*a);
-}
-
-vec3 color(const ray& r) {
-    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
-    if(t > 0.0)
-    {
-        vec3 N = normalize(r.point_at_parameter(t) - vec3(0,0,-1));
-        return vec3(0.5)*(N+vec3(1));
+vec3 color(const ray& r, hitable* world) {
+    hit_record rec;
+    if(world->hit(r, 0.0, MAXFLOAT, rec)) {
+        return vec3(0.5)*(rec.normal+vec3(1));
     }
-    vec3 unit_direction = normalize(r.direction());
-    t = 0.5 * (unit_direction.y + 1);
-    return (1-t)*vec3(1) + t*vec3(0.5, 0.7, 1.0);
+    else {
+        vec3 unit_direction = normalize(r.direction());
+        float t = 0.5 * (unit_direction.y + 1);
+        return (1-t)*vec3(1) + t*vec3(0.5, 0.7, 1.0);
+    }
 }
 
 void redraw(U8* outPtr, int width, int height) {
@@ -29,12 +19,18 @@ void redraw(U8* outPtr, int width, int height) {
     vec3 horizontal(4, 0, 0);
     vec3 vertical(0, 2, 0);
     vec3 origin(0);
+    
+    hitable* list[2];
+    list[0] = new sphere(vec3(0,0,-1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable* world = new hitable_list(list, 2);
+    
     for(auto j = 0; j < height; j++) {
         for(auto i = 0; i < width; i++) {
             float u = float(i) / width;
             float v = float(j) / height;
             ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
+            vec3 col = color(r, world);
             *(outPtr++) = U8(255.99 * col.r);
             *(outPtr++) = U8(255.99 * col.g);
             *(outPtr++) = U8(255.99 * col.b);
---
+++ raytracer/sphere.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "hitable.h"
+
+struct sphere : public hitable {
+    sphere() {};
+    sphere(vec3 cen, float r) : center(cen), radius(r) {}
+    
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+    
+    vec3 center;
+    float radius;
+};
