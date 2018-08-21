f0d8493a4c8b1ec8c6e46b12a11c868ad252f6b8
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-21 13:38:19 +0100

    Simplify lambertian scatter, apparently no perf diff tho.

--- raytracer/materials/lambertian.cpp
+++ raytracer/materials/lambertian.cpp
@@ -2,8 +2,8 @@
 #include "glm/gtc/random.hpp"
 
 bool lambertian::scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
-    vec3 target = rec.p + rec.normal + ballRand(1.0f);
-    scattered = ray(rec.p, target-rec.p, r_in.time());
+    vec3 target = rec.normal + ballRand(1.0f);
+    scattered = ray(rec.p, target, r_in.time());
     attenuation = albedo;
     return true;
 }
