0948a9e58917228c6da3517bcc4ca19a8e80fb74
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-26 01:52:01 +0100

    perlin stage 2

--- raytracer/perlin.cpp
+++ raytracer/perlin.cpp
@@ -1,13 +1,35 @@
 #include "perlin.h"
 
+inline float trilinear_interp(float c[2][2][2], float u, float v, float w) {
+    float accum = 0;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            for(int k = 0; k < 2; k++) {
+                accum += (i*u + (1-i)*(1-u)) *
+                         (j*v + (1-j)*(1-v)) *
+                         (k*w + (1-k)*(1-w)) * c[i][j][k];
+            }
+        }
+    }
+    return accum;
+}
+
 float perlin::noise(const vec3& p) const {
-    float u = p.x - floor(p.x);
-    float v = p.y - floor(p.y);
-    float w = p.z - floor(p.z);
-    int i = int(4*p.x) & 255;
-    int j = int(4*p.y) & 255;
-    int k = int(4*p.z) & 255;
-    return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+    int i = floor(p.x);
+    int j = floor(p.y);
+    int k = floor(p.z);
+    float u = p.x - i;
+    float v = p.y - j;
+    float w = p.z - k;
+    float c[2][2][2];
+    for(int di = 0; di < 2; di++) {
+        for(int dj = 0; dj < 2; dj++) {
+            for(int dk = 0; dk < 2; dk++) {
+                c[di][dj][dk] = ranfloat[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
+            }
+        }
+    }
+    return trilinear_interp(c, u, v, w);
 }
 
 static float* perlin_generate() {
