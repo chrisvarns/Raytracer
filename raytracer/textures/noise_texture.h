50ee0687b74e4d9f1b1cf5f658fd04dcc1274ea8
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-26 02:44:47 +0100

    Perlin chapter done!

--- raytracer/textures/noise_texture.h
+++ raytracer/textures/noise_texture.h
@@ -6,7 +6,8 @@ struct noise_texture : public texture {
     noise_texture(float scale) : scale_(scale) {}
     vec3 value(float u, float v, const vec3& p) const {
         //return vec3(1) * 0.5f * (1.0f + noise_.turb(scale_ * p));
-        return vec3(1) * (noise_.turb(scale_ * p));
+        //return vec3(1) * (noise_.turb(scale_ * p));
+        return vec3(1) * 0.5f * (1 + sin(scale_*p.z + 10*noise_.turb(p)));
     }
 
     perlin noise_;
