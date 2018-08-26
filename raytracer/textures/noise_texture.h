f3ec73d6838003e09f6c7ed85763e632f8bc9d2b
Chris Varnsverry <chrisvarnz@gmail.com>
2018-08-26 02:32:34 +0100

    Fixed by reading ahead, the book can be a bit out of order

--- raytracer/textures/noise_texture.h
+++ raytracer/textures/noise_texture.h
@@ -5,7 +5,7 @@
 struct noise_texture : public texture {
     noise_texture(float scale) : scale_(scale) {}
     vec3 value(float u, float v, const vec3& p) const {
-        return vec3(1) * noise_.noise(scale_ * p);
+        return vec3(1) * 0.5f * (1.0f + noise_.noise(scale_ * p));
     }
 
     perlin noise_;
